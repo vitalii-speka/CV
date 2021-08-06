10f6f54467a294565065b04f824da12af29e50f1
vitalii-speka <65010140+vitalii-speka@users.noreply.github.com>
2021-08-06 17:05:21 +0300

    add style Projects

--- src/Componets/Projects/Projects.jsx
+++ src/Componets/Projects/Projects.jsx
@@ -3,19 +3,21 @@ import style from './Projects.module.scss';
 function Project({ label, link, tech }) {
   return (
     <li className={style.Project__projectsItem}>
-      <a
-        className={style.Project__projectsName}
-        href={link}
-        target="_blank"
-        rel="noreferrer"
-      >
-        {label}
-      </a>
-      <span className={style.Project__dots}>...........................</span>
-      <span>
-        <b>[</b> <span className={style.Project__techName}>{tech}</span>
-        <b> ]</b>
-      </span>
+      <div className={style.Project__flex}>
+        <a
+          className={style.Project__projectsName}
+          href={link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {label}
+        </a>
+        {/* <span className={style.Project__dots}>...........</span> */}
+        <span>
+          <b>[</b> <span className={style.Project__techName}>{tech}</span>
+          <b> ]</b>
+        </span>
+      </div>
     </li>
   );
 }
